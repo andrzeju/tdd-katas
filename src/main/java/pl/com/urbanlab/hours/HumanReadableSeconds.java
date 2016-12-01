cd998c61740e2edfacf0c38baa4e0b8678322908
Andrzej <andrzej@localhost>
2016-12-01 22:24:27 +0100

    human readable time refactor: magic nums, constants and redundand ifs

--- src/main/java/pl/com/urbanlab/hours/HumanReadableSeconds.java
+++ src/main/java/pl/com/urbanlab/hours/HumanReadableSeconds.java
@@ -4,13 +4,23 @@ package pl.com.urbanlab.hours;
  * Created by andrzej on 01.12.16.
  */
 public class HumanReadableSeconds {
+
+    public static final String SECONDS_SUFFIX = "s";
+    public static final String MINUTES_SUFFIX = "m:";
+    public static final String HOURS_SUFFIX = "h:";
+
     public String convertSeconds(long seconds) {
-        if (seconds < 60) {
-            return seconds + "s";
+        boolean minutesAndSeconds = seconds >= 60 && seconds < 3600;
+        boolean hoursMinutesAndSeconds = seconds >= 3600;
+
+        String result = "";
+        if (minutesAndSeconds) {
+            result = seconds / 60 + MINUTES_SUFFIX;
         }
-        if (seconds < 3600) {
-            return seconds / 60 + "m:" + seconds % 60 + "s";
+        if (hoursMinutesAndSeconds) {
+            result = seconds / 3600 + HOURS_SUFFIX + (seconds % 3600 - seconds % 60) / 60 + MINUTES_SUFFIX;
         }
-        return seconds / 3600 + "h:" + (seconds % 3600 - seconds % 60) / 60 + "m:" + seconds % 60 + "s";
+        result += seconds % 60 + SECONDS_SUFFIX;
+        return result;
     }
 }
