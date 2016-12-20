50ab6e12789f94a31d1ca293450b1fee7783b891
andrzeju <andrzej.urban.pl@gmail.com>
2016-12-20 21:19:56 +0100

    bowling kata - Game class and test

---
+++ src/main/java/pl/com/urbanlab/bowling/Game.java
@@ -0,0 +1,14 @@
+package pl.com.urbanlab.bowling;
+
+/**
+ * Created by andrzej on 20.12.16.
+ */
+public class Game {
+    public void roll(int pins) {
+
+    }
+
+    public int score() {
+
+    }
+}
---
+++ src/test/java/pl/com/urbanlab/bowling/GameTest.java
@@ -0,0 +1,10 @@
+package pl.com.urbanlab.bowling;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by andrzej on 20.12.16.
+ */
+public class GameTest {
+
+}
