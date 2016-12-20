138947521e9431270d3b3556b70febff7ec26269
andrzeju <andrzej.urban.pl@gmail.com>
2016-12-20 21:38:45 +0100

    bowling - test game and basic rolls summary

--- src/main/java/pl/com/urbanlab/bowling/Game.java
+++ src/main/java/pl/com/urbanlab/bowling/Game.java
@@ -4,11 +4,14 @@ package pl.com.urbanlab.bowling;
  * Created by andrzej on 20.12.16.
  */
 public class Game {
-    public void roll(int pins) {
 
+    private int score = 0;
+
+    public void roll(int pins) {
+        score += pins;
     }
 
     public int score() {
-
+        return score;
     }
 }
--- src/test/java/pl/com/urbanlab/bowling/GameTest.java
+++ src/test/java/pl/com/urbanlab/bowling/GameTest.java
@@ -1,10 +1,36 @@
 package pl.com.urbanlab.bowling;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by andrzej on 20.12.16.
  */
 public class GameTest {
+
+    private Game game;
+
+    @Before
+    public void setUp() {
+        game = new Game();
+    }
+
+    @Test
+    public void testGame() {
+        for (int i=0; i<20; i++) {
+            game.roll(0);
+        }
+        assertEquals(0, game.score());
+    }
+
+    @Test
+    public void testAllOnes() {
+        for (int i = 0; i < 20; i++) {
+            game.roll(1);
+        }
+        assertEquals(20, game.score());
+    }
 
 }
